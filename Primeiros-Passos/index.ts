120ee8c352af8050898ab0998a8bf759877918a6
Oieusouopi <rafasc866@gmail.com>
2022-05-31 10:47:15 -0300

    first commit

---
+++ Primeiros-Passos/index.ts
@@ -0,0 +1,25 @@
+//Type Boolean
+let yes: boolean = true;
+let no: boolean = false;
+
+//Type Number
+let x: number;
+
+let y: number = 0;
+let z: number = 123.456;
+
+//Type String
+let s: string;
+
+let b: string = '';
+let r: string = 'rafael';
+let a: string = 'b';
+
+//Type void
+function helloWorld(): void {
+    console.log('Hello World!');
+};
+
+//Type Null e Undefined
+const nullValue = null;
+const undefinedValue = undefined;

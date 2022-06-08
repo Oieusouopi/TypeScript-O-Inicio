70647e19a4c03e8f42e235dbe95d7351d632949c
Oieusouopi <rafasc866@gmail.com>
2022-06-08 15:27:34 -0300

    rainbowColors

---
+++ Enum/dist/rainbowColors.js
@@ -0,0 +1,18 @@
+var rainbowColors;
+(function (rainbowColors) {
+    rainbowColors["red"] = "vermelho";
+    rainbowColors["orange"] = "laranja";
+    rainbowColors["yellow"] = "amarelo";
+    rainbowColors["green"] = "verde";
+    rainbowColors["blue"] = "azul";
+    rainbowColors["anil"] = "anil";
+    rainbowColors["violet"] = "violeta";
+})(rainbowColors || (rainbowColors = {}));
+;
+console.log(rainbowColors.red);
+console.log(rainbowColors.orange);
+console.log(rainbowColors.yellow);
+console.log(rainbowColors.green);
+console.log(rainbowColors.blue);
+console.log(rainbowColors.anil);
+console.log(rainbowColors.violet);
---
+++ Enum/rainbowColors.ts
@@ -0,0 +1,17 @@
+enum rainbowColors {
+    red = 'vermelho',
+    orange = 'laranja',
+    yellow = 'amarelo',
+    green = 'verde',
+    blue = 'azul',
+    anil = 'anil',
+    violet = 'violeta',
+};
+
+console.log(rainbowColors.red);
+console.log(rainbowColors.orange);
+console.log(rainbowColors.yellow);
+console.log(rainbowColors.green);
+console.log(rainbowColors.blue);
+console.log(rainbowColors.anil);
+console.log(rainbowColors.violet);

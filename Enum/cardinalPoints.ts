00213abef1eb5a3532988091310f6e7afd35af85
Oieusouopi <rafasc866@gmail.com>
2022-06-08 15:35:43 -0300

    cardinalPoints

---
+++ Enum/cardinalPoints.ts
@@ -0,0 +1,11 @@
+enum cardinalPoints {
+    north = 'N',
+    east = 'E',
+    west = 'W',
+    south = 'S',
+};
+
+console.log(cardinalPoints.north);
+console.log(cardinalPoints.east);
+console.log(cardinalPoints.west);
+console.log(cardinalPoints.south);

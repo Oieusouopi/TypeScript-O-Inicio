3b7f705c71bfcc4e1e22b0e01a8f1ca9b3632f2b
Oieusouopi <rafasc866@gmail.com>
2022-06-08 15:45:39 -0300

    arrayTypes js

---
+++ ArrayTypeScript/dist/array.js
@@ -0,0 +1,5 @@
+var names = ['rafael', 'gabriel', 'jhonathan', 'luiz'];
+var pages = [1, 2, 3, 4, 5, 6, 7];
+var arrayBooleans = [true, false, true, true];
+var arrayUndefined = [undefined, null, undefined, null];
+var arrayNull = [undefined, null];

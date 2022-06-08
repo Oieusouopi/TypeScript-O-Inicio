3b6f0400e0b583d46598e9bccd465962a6fce25b
Oieusouopi <rafasc866@gmail.com>
2022-06-08 15:45:06 -0300

    arrayTypes

---
+++ ArrayTypeScript/array.ts
@@ -0,0 +1,9 @@
+const names: string[] = ['rafael', 'gabriel', 'jhonathan', 'luiz'];
+
+const pages: number[] = [1,2,3,4,5,6,7];
+
+const arrayBooleans: boolean[] = [true, false, true , true];
+
+const arrayUndefined: undefined[] = [undefined, null, undefined, null];
+
+const arrayNull: null[] = [undefined, null];

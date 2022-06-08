00f6eec7645d6dcd7e7bb7ede81852a49103722c
Oieusouopi <rafasc866@gmail.com>
2022-06-08 15:36:20 -0300

    carinalPoints js

---
+++ Enum/dist/cardinalPoints.js
@@ -0,0 +1,12 @@
+var cardinalPoints;
+(function (cardinalPoints) {
+    cardinalPoints["north"] = "N";
+    cardinalPoints["east"] = "E";
+    cardinalPoints["west"] = "W";
+    cardinalPoints["south"] = "S";
+})(cardinalPoints || (cardinalPoints = {}));
+;
+console.log(cardinalPoints.north);
+console.log(cardinalPoints.east);
+console.log(cardinalPoints.west);
+console.log(cardinalPoints.south);

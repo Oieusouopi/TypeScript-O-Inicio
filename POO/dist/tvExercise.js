4606a2259cbbf3841d1e5344495afe3d6322ed1e
Oieusouopi <rafasc866@gmail.com>
2022-06-20 13:57:53 -0300

    connectTo e um metodo setConnectedTo

--- POO/dist/tvExercise.js
+++ POO/dist/tvExercise.js
@@ -1,15 +1,20 @@
 "use strict";
 class tv {
-    // connectedTo: string;
     constructor(brand, size, resolution, connections) {
         this.brand = brand;
         this.size = size;
         this.resolution = resolution;
         this.connections = connections;
+        this.connectedTo = null;
+    }
+    setConnectedTo(connectedTo) {
+        this.connectedTo = connectedTo;
     }
     turnOn() {
-        console.log(`Marca: ${this.brand}, Tamanho: ${this.size}in, Resolução: ${this.resolution}, Conexões: ${this.connections}`);
+        console.log(`Marca: ${this.brand}, Tamanho: ${this.size}in, Resolução: ${this.resolution}, Conexões: ${this.connections}, Conexão atual: ${this.connectedTo}`);
     }
 }
 const consul = new tv('consul', 158, '720p', ['HDMI', 'Ethernet', 'USB']);
+consul.turnOn();
+consul.setConnectedTo('Ethernet');
 consul.turnOn();
--- POO/tvExercise.ts
+++ POO/tvExercise.ts
@@ -3,19 +3,26 @@ class tv {
     size: number;
     resolution: string;
     connections: string[];
-    // connectedTo: string;
+    connectedTo: string | null;
 
     constructor(brand: string, size: number, resolution: string, connections: string[]) {
         this.brand = brand;
         this.size = size;
         this.resolution = resolution;
         this.connections = connections;
+        this.connectedTo = null;
+    }
+    
+    setConnectedTo(connectedTo: string) {
+        this.connectedTo = connectedTo;
     }
 
     turnOn() {
-        console.log(`Marca: ${this.brand}, Tamanho: ${this.size}in, Resolução: ${this.resolution}, Conexões: ${this.connections}`);
+        console.log(`Marca: ${this.brand}, Tamanho: ${this.size}in, Resolução: ${this.resolution}, Conexões: ${this.connections}, Conexão atual: ${ this.connectedTo }`);
     }
 }
 
 const consul = new tv('consul', 158, '720p', ['HDMI', 'Ethernet', 'USB'] );
+consul.turnOn();
+consul.setConnectedTo('Ethernet');
 consul.turnOn();
